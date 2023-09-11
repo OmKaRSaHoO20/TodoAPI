f2640449193ebacc66d943ff59ec64627a96fbdb
Omkar Sahoo <94987259+OmKaRSaHoO20@users.noreply.github.com>
2023-09-11 23:49:20 +0530

    Updates

--- controllers/user.js
+++ controllers/user.js
@@ -2,10 +2,8 @@ import jwt from "jsonwebtoken";
 import User from "../models/user.js";
 
 function generateJWTToken(password) {
-  // Define the payload containing the user ID
   const payload = { password };
 
-  // Sign the JWT token with a secret key and set an expiration time of 30 days
   const token = jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: "1d" });
 
   return token;
@@ -14,18 +12,15 @@ function generateJWTToken(password) {
 export const loginUser = async (req, res, next) => {
   const { email, password } = req.body;
 
-  // Invalid request if params are empty
   if (!password || !email) {
     return res.status(400).json({ message: "Invalid request" });
   }
 
-  let user; // Declare the user variable here
+  let user;
 
   try {
-    // Check if the user exists in the database
     user = await User.findOne({ email });
     if (!user) {
-      // Create a new user if not found
       const token = generateJWTToken(password);
       user = new User({ email, password });
       await user.save();
